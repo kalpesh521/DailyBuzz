93773036b9f88eb78af3342c9f0911b4b562bcb6
Kalpesh Pawar <kalpeshpawar7875@gmail.com>
2022-02-07 19:52:14 +0530

    ui

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from newsapp import views
 
 urlpatterns = [
-    
+    path('', views.index, name='index'),
     path('', views.home, name='home'),
-#     path('', views.about, name='about'),
-#     path('', views.contact, name='contact'),
+    path('', views.about, name='about'),
+    path('', views.contact, name='contact'),
 ]
